286711815fc8d47c16f33da7a5095652ef056252
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-03-18 11:13:55 -0300

    Update 10porcento.c

--- 10porcento.c
+++ 10porcento.c
@@ -5,15 +5,15 @@ int main() {
 	
 	setlocale(LC_ALL, "portuguese");
 	
-	int valor, desconto;
+	int valor, desconto, valorFinal;
 	
 	printf("Digite o valor do produto: ");
 	scanf("%i", &valor);
 	
 	desconto = valor * 0.10;
-	
+	valorFinal = valor - desconto;
 	printf("Valor do desconto:%i\n", desconto);
-	
+	printf("Valor final do produto:%i\n", valorFinal);
 	return 0;
 	
 }
