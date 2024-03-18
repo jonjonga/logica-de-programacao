aa56039e0fdff0689ad50b6fb2222ecd7847befd
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-03-18 09:08:14 -0300

    Add files via upload

---
+++ media em c.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	
+	setlocale(LC_ALL, "portuguese");
+	
+	char nome[200];
+	int idade;
+	float primeiraNota, segundaNota, terceiraNota, quartaNota;
+	float media;
+	
+	printf("Digite o nome do aluno: ");
+	scanf("%s",&nome);
+	
+	printf("Digite a idade do aluno: ");
+	scanf("%i",&idade);
+	
+	printf("Digite a primeira nota: ");
+	scanf("%f",&primeiraNota);
+	
+	printf("Digite a segunda nota: ");
+	scanf("%f",&segundaNota);
+	
+	printf("Digite a terceira nota: ");
+	scanf("%f",&terceiraNota);
+	
+	printf("Digite a quarta nota: ");
+	scanf("%f",&quartaNota);
+	
+	media = (primeiraNota + segundaNota + terceiraNota + quartaNota) / 4;
+	
+	printf("Nome do aluno:%s\n",nome);
+	printf("\nIdade do aluno:%i\n",idade);
+	printf("\nNota da primeira unidade:%f\n",primeiraNota);
+	printf("\nNota da segunda unidade:%f\n",segundaNota);
+	printf("\nNota da terceira unidade:%f\n",terceiraNota);
+	printf("\nNota da quarta unidade:%f\n",quartaNota);
+	printf("\nMédia:%f\n",media);
+}
