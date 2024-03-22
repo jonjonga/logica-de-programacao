9fc7d8e863e7e71f61cbc4f6200e1f23a13d8085
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-03-22 10:57:02 -0300

    Add files via upload

---
+++ peso.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "");
+	
+	char sexo;
+	float peso, altura;
+	
+	printf("Digite seu sexo: ");
+	scanf("%c", &sexo);
+	
+	printf("Digite sua altura: ");
+	scanf("%f", &altura);
+	
+	system("cls||clear");
+	
+	fflush(stdin);
+	
+	switch (sexo) {
+		case 'M':
+		peso = (72.7 * altura) - 58;
+		printf("=== Seu peso ideal é: %.2f ", peso);
+		break;
+	case 'F':
+	peso = (62.1 * altura) - 44.7;
+	printf("=== Seu peso ideal é: %.2f ", peso);
+	break;
+	default:
+		printf("=== Opção inválida ====");
+	}
+
+}
