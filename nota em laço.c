b8d1d76be520203640020ac8f7ad5b3d8d388f08
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-03-25 11:22:12 -0300

    Add files via upload

---
+++ nota em laço.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <locale.h>
+int main() {
+	setlocale(LC_ALL, "");
+	
+	float notas, soma, media;
+	int i;
+	
+	for (i = 1; i <= 4; i++){
+		printf("Digite a %iº Nota:", i);
+		scanf("%f", &notas);
+		
+		soma = soma + notas;
+	
+	}
+	media = soma / 4;
+	printf("\n=== Exibindo resultados ===\n");
+	printf("Média %.1f \n", media);
+}
