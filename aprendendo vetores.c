34f2963c1aded66e11578f821db27d1b3650e48f
jonjonga <161032162+jonjonga@users.noreply.github.com>
2024-04-09 08:35:04 -0300

    Add files via upload

---
+++ aprendendo vetores.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	
+	int numeros[5];
+	
+	numeros[0] = 1;
+	numeros[1] = 2;
+	numeros[2] = 3;
+	numeros[3] = 4;
+	numeros[4] = 5;
+	
+	printf("elemento 1: %d\n", numeros [0]);
+	printf("elemento 2: %d\n", numeros [1]);
+	printf("elemento 3: %d\n", numeros [2]);
+	printf("elemento 4: %d\n", numeros [3]);
+	printf("elemento 5: %d\n", numeros [4]);
+
+	
+}
